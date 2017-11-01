1e6908fc0c9d4c23435cf7ed9277e05e593e091a
regpu <regpu@TITANIUM.Home>
2017-11-01 13:39:48 -0600

    dfsafdsf

--- src/DevTSK/Units/Viewer.java
+++ src/DevTSK/Units/Viewer.java
@@ -17,24 +17,24 @@ public class Viewer extends JPanel {
 
 	public Viewer(Map map) {
 		this.map = map;
-		tileset = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("assets/tiles/tileset.png"));
+		tileset = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("assets/tile/tile.png"));
 	}
 
 	@Override
-    protected void paintComponent(Graphics g) {
-        g.setColor(new Color(0,0,0));
-        g.fillRect(0, 0, getWidth(), getHeight());
- 
-        for(int i=0; i<map.getTiles().length;i ++)
-            for(int j=0;j&lt;10;j++)
-                drawTile(g, map[j][i], i*tW,j*tH);
-    }
+	protected void paintComponent(Graphics g) {
+		g.setColor(new Color(0, 0, 0));
+		g.fillRect(0, 0, getWidth(), getHeight());
+
+		for (int i = 0; i < map.getTiles().length; i++)
+			for (int o = 0; o < map.getTiles().length; o++)
+				drawTile(g, map.getTiles()[i][o].getTilename(), i * w, o * h);
+	}
 
 	protected void drawTile(Graphics g, TileName t, int x, int y) {
 		// map Tile from the tileset
 		int mx = t.ordinal() % 10;
 		int my = t.ordinal() / 10;
-		g.drawImage(tileset, x, y, x + tW, y + tH,
-				mx * tW, my * tH, mx * tW + tW, my * tH + tH, this);
+		g.drawImage(tileset, x, y, x + w, y + h,
+				mx * w, my * h, mx * w + w, my * h + h, this);
 	}
 }
